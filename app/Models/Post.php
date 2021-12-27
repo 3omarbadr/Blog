11dffa7c894b0356b24d2d17398d45f5d959de33
3omarbadr <omarbadr167@gmail.com>
2021-12-27 04:06:48 +0200

    Use clockwork to show database queries

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,10 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
