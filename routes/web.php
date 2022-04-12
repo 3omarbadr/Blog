e999750f6a7aef5b796031896d82c585a67d88b8
3omarbadr <omarbadr167@gmail.com>
2022-04-12 02:10:34 +0200

    Extract a Dropdown Blade Component

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PostController::class , 'index']);
+Route::get('/', [PostController::class , 'index'])->name('home');
 
 
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -35,7 +35,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'categories' => Category::all()
     ]);
 
-});
+})->name('category');
 
 
 Route::get('authors/{author:username}', function (User $author) {
