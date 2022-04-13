2afe83d199aacbf5412e726b14fdfc5285a5e957
3omarbadr <omarbadr167@gmail.com>
2022-04-13 02:12:14 +0200

    Search (The Messy Way)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,8 +15,14 @@ class PostController extends Controller
      */
     public function index()
     {
+        $posts = Post::latest();
+        if(request('search')){
+            $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+        }
         return view('posts', [
-            'posts' => Post::latest()->get(),
+            'posts' => $posts->get(),
             'categories' => Category::all()
         ]);
     }
