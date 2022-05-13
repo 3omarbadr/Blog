7e55d22afb9fa369de8b98cff3cbe13de376e489
3omarbadr <omarbadr167@gmail.com>
2022-05-13 18:44:28 +0200

    Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+<article class="flex bg-gray-100 p-6 
+border border-gray-200 rounded-xl space-x-4">
+
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Omar Badr</h3>
+            <p class="text-xs">Posted 3 days ago</p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae voluptatem
+            molestiae iure voluptates sequi asperiores, a eos dolor. Amet quod molestias deserunt
+            exercitationem quisquam alias nihil aperiam provident eveniet vero!
+        </p>
+    </div>
+</article>
